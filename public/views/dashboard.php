0852402ca1e9e6bc33a2bcd1168bdca8bd23337a
hnderwn <142008072+hnderwn@users.noreply.github.com>
2025-06-30 06:35:37 +0700

    feat: Menambahkan 4 kartu akses cepat baru di dashboard

--- public/views/dashboard.php
+++ public/views/dashboard.php
@@ -50,31 +50,55 @@
     </div>
     <hr>
     <h5 class="mb-3">Akses Cepat</h5>
-    <div class="row g-4">
-      <div class="col-lg-3 col-md-6">
-          <a href="index.php?page=maintenance_create" class="card card-box shadow-sm text-center p-3">
-              <i class="bi bi-tools fs-1 text-info mb-2"></i>
-              <h6 class="mb-0">Input Maintenance</h6>
-          </a>
-      </div>
-      <div class="col-lg-3 col-md-6">
-          <a href="index.php?page=kerusakan_create" class="card card-box shadow-sm text-center p-3">
-              <i class="bi bi-bug-fill fs-1 text-danger mb-2"></i>
-              <h6 class="mb-0">Lapor Kerusakan</h6>
-          </a>
-      </div>
-       <div class="col-lg-3 col-md-6">
-          <a href="index.php?page=komponen_dipakai_create" class="card card-box shadow-sm text-center p-3">
-              <i class="bi bi-cart-plus-fill fs-1 text-success mb-2"></i>
-              <h6 class="mb-0">Input Komponen Dipakai</h6>
-          </a>
-      </div>
-       <div class="col-lg-3 col-md-6">
-          <a href="index.php?page=teknisi_create" class="card card-box shadow-sm text-center p-3">
-              <i class="bi bi-person-plus-fill fs-1 text-primary mb-2"></i>
-              <h6 class="mb-0">Tambah Teknisi</h6>
-          </a>
-      </div>
+    <div class="row g-3">
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=maintenance_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-tools fs-1 text-info mb-2"></i>
+                <h6 class="mb-0">Input Maintenance</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=kerusakan_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-bug-fill fs-1 text-danger mb-2"></i>
+                <h6 class="mb-0">Lapor Kerusakan</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=komponen_dipakai_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-cart-plus-fill fs-1 text-success mb-2"></i>
+                <h6 class="mb-0">Input Komponen Dipakai</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=teknisi_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-person-plus-fill fs-1 text-primary mb-2"></i>
+                <h6 class="mb-0">Tambah Teknisi</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=cctv_unit_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-camera-reels-fill fs-1 text-secondary mb-2"></i>
+                <h6 class="mb-0">Tambah Unit CCTV</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=cctv_tipe_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-tags-fill fs-1 text-warning mb-2"></i>
+                <h6 class="mb-0">Tambah Tipe CCTV</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=komponen_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-box-seam-fill fs-1 text-dark mb-2"></i>
+                <h6 class="mb-0">Tambah Komponen</h6>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-3">
+            <a href="index.php?page=user_create" class="card card-box shadow-sm text-center p-3">
+                <i class="bi bi-person-lock fs-1 text-primary-emphasis mb-2"></i>
+                <h6 class="mb-0">Tambah User</h6>
+            </a>
+        </div>
     </div>
 </main>
 
